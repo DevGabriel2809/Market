f14e81e58b9617e26fa93d3a25a9d26796b7ea40
Gabriel Andrade <nexy.2809@gmail.com>
2026-04-27 00:04:03 -0300

    2º commit, added backgrounds and js fixing modal cards

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+// SELECIONAR GERENTE
+
 const botoesPersonagem = document.querySelectorAll(".character-option");
 const personagemJogo = document.getElementById("player-character");
 
@@ -19,3 +21,44 @@ botoesPersonagem.forEach((botao) => {
     }
   });
 });
+
+
+// CARDS INICIAIS
+
+const telaMenu = document.getElementById("tela-menu")
+const telaComoJogar = document.getElementById("tela-como-jogar")
+const telaCreditos = document.getElementById("tela-creditos")
+const telaJogo = document.getElementById("tela-jogo")
+
+const btnIniciar = document.getElementById("btn-iniciar")
+const btnComoJogar = document.getElementById("btn-como-jogar")
+const btnCreditos = document.getElementById("btn-creditos")
+const btnVoltarMenu = document.querySelectorAll(".btn-voltar-menu")
+
+function mostrarTela(tela){
+    const telas = document.querySelectorAll(".screen");
+
+    telas.forEach((item) => {
+        item.classList.remove("active")
+    })
+
+    tela.classList.add("active")
+}
+
+btnIniciar.addEventListener("click", () =>{
+    mostrarTela(telaJogo)
+})
+
+btnComoJogar.addEventListener("click", () =>{
+    mostrarTela(telaComoJogar)
+})
+
+btnCreditos.addEventListener("click", () =>{
+    mostrarTela(telaCreditos)
+})
+
+btnVoltarMenu.forEach((botao) => {
+    botao.addEventListener("click", () =>{
+        mostrarTela(telaMenu)
+    })
+})
